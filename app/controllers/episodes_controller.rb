e9862cdfb87c2c80b308c6eaa757ac190acae464
Henry Hadlow <henry@manofscience.co.uk>
2015-01-24 22:02:01 +0000

    Return ids to feedly API to get full details

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -7,13 +7,23 @@ class EpisodesController < ApplicationController
     feedly_user_id = Rails.application.secrets.feedly_user_id
     feedly_access_token = Rails.application.secrets.feedly_access_token
 
-    url = "https://cloud.feedly.com/v3/streams/ids?streamId=user/" + feedly_user_id + "/tag/global.saved"
+    url_starred_ids = "https://cloud.feedly.com/v3/streams/ids?streamId=user/" + feedly_user_id + "/tag/global.saved"
 
     # Get a list of starred ids from feedly API using my credentials
-    starred_ids = HTTParty.get(url.to_str,
+    starred_ids = HTTParty.get(url_starred_ids,
     :headers => { 'Content-Type' => 'application/json',
                   'Authorization' => feedly_access_token } )
 
-    @output = JSON.pretty_generate(starred_ids)
+    body = { "ids" => starred_ids["ids"] }.to_json
+    url_starred_items = "https://cloud.feedly.com/v3/entries/.mget"
+
+    # Get full details of starred ids
+    starred_items = HTTParty.post(url_starred_items,
+    :body => body,
+    :headers => { 'Content-Type' => 'application/json' } )
+
+    @output = starred_items
   end
+
+  private
 end
