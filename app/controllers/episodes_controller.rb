695cf9f2ac351845c79de48c6b7f4956e577b401
Henry Hadlow <henry@manofscience.co.uk>
2015-01-24 11:16:44 +0000

    Add test code to episodes#index Call foursquare API and return results to the console Checks Net:HTTP and json are working :)

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,27 @@
 class EpisodesController < ApplicationController
   def index
+    lat = 47.858205
+    lng = 2.294359
+    puts "Latitude: #{lat}, longitude: #{lng}"
+
+    # Access foursquare API using latitude and longitude
+    url = "https://api.foursquare.com/v2/venues/search
+           ?client_id = WU2GPFLW4FXM1M0Z3AACODQNF3BLOCW5B3NYXFRT1LUOQZ4T
+           &client_secret = 1LYYB0DHQWNYKJ35NJSYTQRKYBOSYQVJV4RUFBRMOOR1QDSP
+           &v=20130815
+           &ll=#{lat},#{lng}"
+    url.delete!(" ").delete!("\n")
+    uri = URI(url)
+
+    # Access the data returned from foursquare
+    json = Net::HTTP.get(uri)
+    response = JSON.parse(json)
+
+    # f = File.new("output.txt", "w+")
+    # f.write response.to_yaml
+
+    venue = response["response"]["venues"][0]
+    puts venue["name"]
   end
 
   def import
