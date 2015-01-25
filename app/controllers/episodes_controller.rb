3d667ffb9f424511acf62005ebde6104093acad6
Henry Hadlow <henry@manofscience.co.uk>
2015-01-25 20:09:22 +0000

    Store the Episodes in the db

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -7,23 +7,35 @@ class EpisodesController < ApplicationController
     feedly_user_id = Rails.application.secrets.feedly_user_id
     feedly_access_token = Rails.application.secrets.feedly_access_token
 
-    url_starred_ids = "https://cloud.feedly.com/v3/streams/ids?streamId=user/" + feedly_user_id + "/tag/global.saved"
-
     # Get a list of starred ids from feedly API using my credentials
+    url_starred_ids = "https://cloud.feedly.com/v3/streams/ids?streamId=user/" + feedly_user_id + "/tag/global.saved"
     starred_ids = HTTParty.get(url_starred_ids,
     :headers => { 'Content-Type' => 'application/json',
                   'Authorization' => feedly_access_token } )
 
-    body = { "ids" => starred_ids["ids"] }.to_json
-    url_starred_items = "https://cloud.feedly.com/v3/entries/.mget"
+    # Compare ids to the database and remove duplicates
 
     # Get full details of starred ids
-    starred_items = HTTParty.post(url_starred_items,
+    body = { "ids" => starred_ids["ids"] }.to_json
+    url_starred_episodes = "https://cloud.feedly.com/v3/entries/.mget"
+    starred_episodes = HTTParty.post(url_starred_episodes,
     :body => body,
     :headers => { 'Content-Type' => 'application/json' } )
 
-    @output = starred_items
+    # Store the episodes in the db
+    starred_episodes.each do |episode|
+      # Select keys to store in db
+      episode_to_store = {
+        :feedly_id => episode["id"],
+        :show => episode["origin"]["title"],
+        :title => episode["title"],
+        :description => episode["summary"]["content"],
+        # and store the whole response as json
+        :json => episode.to_json
+      }
+      Episode.create!(episode_to_store.to_hash)
+    end
+
   end
 
-  private
 end
